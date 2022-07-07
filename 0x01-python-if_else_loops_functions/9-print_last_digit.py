2e58967d350fc5475584f315bcabfaf550cf5603
Yab1 <yabilisanu@gmail.com>
2022-07-07 11:46:34 +0300

    Remove Directories

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        print(f"{-(number % -10)}", end="")
-        return -(number % -10)
-    else:
-        print(f"{number % 10}", end="")
-        return number % 10
